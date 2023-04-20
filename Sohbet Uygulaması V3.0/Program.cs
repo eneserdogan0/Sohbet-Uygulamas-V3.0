924e167e8f961aec6ee6e4cd92df05b8a0a36551
eneserdogan0 <eneserdogan4242@gmail.com>
2023-04-20 23:53:54 +0300

    Proje dosyası ekle.

---
+++ Sohbet Uygulaması V3.0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sohbet_Uygulaması_V3._0
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Uygulamanın ana girdi noktası.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
